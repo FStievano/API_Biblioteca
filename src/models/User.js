bf8f7b14e479d7e37a06f450fc055a9468b5c7bc
Fabrício Stievano <fabriciostievano1999@gmail.com>
2025-10-25 23:30:46 -0300

    Cria modelo e rotas para Usuários

---
+++ src/models/User.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  birthDate: { type: Date, required: true },
+  sex: { type: String, required: true },
+  address: { type: String, required: true }
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
